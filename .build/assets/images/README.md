09a46dc39324b72d77235db985a67c16dbbe2655
Gabrielle Iskandar <gabiskandar@gmail.com>
2018-04-30 16:01:42 +0300

    Fixed header layout and contents, amended body positions. #4

---
+++ .build/assets/images/README.md
@@ -0,0 +1,3 @@
+# Image separation
+
+All images in this folder are separated firstly in order of format and further separated by concern.
---
+++ .build/assets/scripts/js/aether.js
@@ -0,0 +1,10 @@
+webpackJsonp([2],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+
+/***/ })
+],[0]);
